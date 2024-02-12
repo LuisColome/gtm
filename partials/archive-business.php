b990e05678c82042aff1807f303b6e5f385d15b4
Luis Colomé <hello@luiscolome.com>
2024-02-12 18:24:31 +0100

    New custom fields for button on directory listings

--- partials/archive-business.php
+++ partials/archive-business.php
@@ -37,7 +37,14 @@
                 <div class="business__links">
                     
                     <div class="business__link">
+                        <?php                    
+                    $button_text = get_field('drtr_button_text', $post->ID);
+                    $button_link = get_field('drtr_button_link', $post->ID);
+                    if( $button_link ){?>
+                        <a class="kb-button kt-button button kt-btn-size-standard kt-btn-width-type-auto kb-btn-global-inherit kt-btn-has-text-true kt-btn-has-svg-false wp-block-button__link wp-block-kadence-singlebtn" href="<?php echo $button_link; ?>"><span class="kt-btn-inner-text"><?php echo $button_text; ?></span></a>
+                    <?php } else {?>
                         <a class="kb-button kt-button button kt-btn-size-standard kt-btn-width-type-auto kb-btn-global-inherit kt-btn-has-text-true kt-btn-has-svg-false wp-block-button__link wp-block-kadence-singlebtn" href="<?php echo get_permalink(); ?>"><span class="kt-btn-inner-text">View more</span></a>
+                    <?php } ?>
                     </div>
                                             
                 </div>
